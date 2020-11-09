4db62a8b462681b15479335d4b9ac7a2f3f2a3b2
yaoyishu <1950193069@qq.com>
2020-11-09 21:16:42 +0800

    新增index-v01.js

--- src/js/index-v01.js
+++ src/js/index-v01.js
@@ -1,40 +1,56 @@
 // 获取DOM元素
-const listInput = document.querySelector("#listInput")
 const todoOl = document.querySelector("#todo")
 const doneOl = document.querySelector("#done")
 const content = document.querySelector(".content")
 const clearbtn = document.querySelector("#clearbtn")
 const todocount = document.querySelector("#todocount")
 const donecount = document.querySelector("#donecount")
-const itemText = document.querySelector(".itemText")
 let todonum = 0;
 let donenum = 0;
 let todoList = [];
 
-// clear 清楚数据
-clearbtn.onclick = function (todoList) {
-  todoList = [];
-  render(todoList)
-  // console.log(todoList);
+// 初始函数
+function init() {
+  // 执行获取数据
+  addData()
+  // 执行清除数据
+  clearData()
+  // 执行存入本地缓存
+  handleStorage()
+
+  todoOl.addEventListener("change", todoEvent)
+  doneOl.addEventListener("change", doneEvent)
+  content.addEventListener("click", conEvent)
+}
+init();
+
+// 清楚数据
+function clearData() {
+  clearbtn.addEventListener('click', function (todoList) {
+    todoList = [];
+    render(todoList)
+  })
 }
 
 // 获取输入框输入的内容
-listInput.onkeydown = function (e) {
-  if (e.key === "Enter") {
-    if (!e.target.value) {
-      alert("请输入清单")
-    } else {
-      const iuputValue = e.target.value;
-      // console.log(e.target.value);
-      // 形成 listItem 对象存放信息
-      const listItem = {
-        title: iuputValue,
-        isDone: false
+function addData() {
+  const listInput = document.querySelector("#listInput")
+  listInput.addEventListener('keydown', function (e) {
+    if (e.key === "Enter") {
+      if (!e.target.value) {
+        alert("请输入清单")
+      } else {
+        const iuputValue = e.target.value;
+        // 形成 listItem 对象存放信息
+        const listItem = {
+          title: iuputValue,
+          isDone: false
+        }
+        todoList.push(listItem)
+        render(todoList)
       }
-      todoList.push(listItem)
-      render(todoList)
     }
-  }
+  })
 }
 
 // 渲染数据
@@ -42,10 +58,10 @@ function render(todoList) {
   // 处理JSON数据 转为 字符串
   localStorage.todoList = JSON.stringify(todoList);
   // 每次渲染前，把<ol id="todo"></ol>\<ol id="done"></ol> 清空后再渲染
-  todocount.innerHTML = 0
-  donecount.innerHTML = 0
   todoOl.innerHTML = ""
   doneOl.innerHTML = ""
+  todocount.innerHTML = 0
+  donecount.innerHTML = 0
   todonum = 0;
   donenum = 0;
   // console.log(todoList);
@@ -87,21 +103,20 @@ function handleStorage() {
   render(todoList)
 }
 
-todoOl.onchange = function (e) {
+// 处理事件
+function todoEvent(e) {
   const index = parseInt(e.target.dataset.index)
   todoList[index].isDone = true;
   render(todoList)
 }
 
-handleStorage()
-
-doneOl.onchange = function (e) {
+function doneEvent(e) {
   const index = parseInt(e.target.dataset.index)
   todoList[index].isDone = false;
   render(todoList)
 }
 
-content.onclick = function (e) {
+function conEvent(e) {
   if (e.target.className === "del") {
     const index = e.target.dataset.index
     todoList.splice(index, 1)
